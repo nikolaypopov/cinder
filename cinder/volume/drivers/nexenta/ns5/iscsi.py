a5624f0a762e83a6ea456547d228631d2762d81f
Qeas <alexey.khodos@nexenta.com>
2017-05-17 00:58:41 +0300

    ns5 failover support

--- cinder/volume/drivers/nexenta/ns5/iscsi.py
+++ cinder/volume/drivers/nexenta/ns5/iscsi.py
@@ -29,7 +29,7 @@ from cinder.volume.drivers.nexenta import options
 from cinder.volume.drivers.nexenta import utils
 import uuid
 
-VERSION = '1.1.0'
+VERSION = '1.2.0'
 LOG = logging.getLogger(__name__)
 TARGET_GROUP_PREFIX = 'cinder-tg-'
 
@@ -40,6 +40,7 @@ class NexentaISCSIDriver(driver.ISCSIDriver,
     """Executes volume driver commands on Nexenta Appliance.
 
     Version history:
+        1.2.0 - Failover support.
         1.1.0 - Added HTTPS support.
                 Added use of sessions for REST calls.
                 Added abandoned volumes and snapshots cleanup.
@@ -70,7 +71,7 @@ class NexentaISCSIDriver(driver.ISCSIDriver,
                 options.NEXENTA_RRMGR_OPTS)
         self.use_https = self.configuration.nexenta_use_https
         self.nef_host = self.configuration.nexenta_rest_address
-        self.vip = self.configuration.nexenta_host
+        self.iscsi_host = self.configuration.nexenta_host
         self.nef_port = self.configuration.nexenta_rest_port
         self.nef_user = self.configuration.nexenta_user
         self.nef_password = self.configuration.nexenta_password
@@ -94,7 +95,7 @@ class NexentaISCSIDriver(driver.ISCSIDriver,
         return backend_name
 
     def do_setup(self, context):
-        host = self.nef_host or self.vip
+        host = self.nef_host or self.iscsi_host
         self.nef = jsonrpc.NexentaJSONProxy(
             host, self.nef_port, self.nef_user,
             self.nef_password, self.use_https)
@@ -112,7 +113,7 @@ class NexentaISCSIDriver(driver.ISCSIDriver,
             else:
                 raise
 
-        # self._fetch_volumes()
+        self._fetch_volumes()
 
     def _fetch_volumes(self):
         url = 'san/iscsi/targets?fields=alias,name&limit=50000'
@@ -287,14 +288,14 @@ class NexentaISCSIDriver(driver.ISCSIDriver,
                 (self._get_volume_path(src_vref), snapshot['name'])))
         except exception.NexentaException:
             LOG.error('Volume creation failed, deleting created snapshot '
-                      '%s', '@'.join(
-                          [snapshot['volume_name'], snapshot['name']]))
+                      '%s', '@'.join([snapshot['volume_name'],
+                                     snapshot['name']]))
             try:
                 self.delete_snapshot(snapshot)
             except (exception.NexentaException, exception.SnapshotIsBusy):
                 LOG.warning('Failed to delete zfs snapshot '
-                            '%s', '@'.join(
-                                [snapshot['volume_name'], snapshot['name']]))
+                            '%s', '@'.join([snapshot['volume_name'],
+                                           snapshot['name']]))
             raise
 
     def create_export(self, _ctx, volume, connector):
@@ -365,10 +366,9 @@ class NexentaISCSIDriver(driver.ISCSIDriver,
         free_amount = utils.str2gib_size(
             stats['bytesAvailable'] - stats['bytesUsed'])
 
-        host = self.vip.split(',')[0] if self.vip else self.nef_host
         location_info = '%(driver)s:%(host)s:%(pool)s/%(group)s' % {
             'driver': self.__class__.__name__,
-            'host': host,
+            'host': self.iscsi_host,
             'pool': self.storage_pool,
             'group': self.volume_group,
         }
@@ -433,7 +433,7 @@ class NexentaISCSIDriver(driver.ISCSIDriver,
 
                 # Create new target
                 url = 'san/iscsi/targets'
-                portal = self.vip.split(',')[0] if self.vip else self.nef_host
+                portal = self.iscsi_host
                 data = {
                     "portals": [
                         {"address": portal}
@@ -465,7 +465,7 @@ class NexentaISCSIDriver(driver.ISCSIDriver,
             except exception.NexentaException as e:
                 if 'No such target group' in e.args[0]:
                     self._create_target_group(tg_name, target_name)
-                    # self._fill_volumes(tg_name)
+                    self._fill_volumes(tg_name)
                     self.nef.post(url, data)
                 else:
                     raise
@@ -479,9 +479,10 @@ class NexentaISCSIDriver(driver.ISCSIDriver,
                 data = self.nef.get(vol_map_url).get('data')
             lun = data[0]['lun']
 
-            host = self.vip.split(',')[0] if self.vip else self.nef_host
+            # host = self.vip.split(',')[0] if self.vip else self.nef_host
+            # LOG.warning(host)
             provider_location = '%(host)s:%(port)s,1 %(name)s %(lun)s' % {
-                'host': host,
+                'host': self.iscsi_host,
                 'port': self.configuration.nexenta_iscsi_target_portal_port,
                 'name': target_name,
                 'lun': lun,
