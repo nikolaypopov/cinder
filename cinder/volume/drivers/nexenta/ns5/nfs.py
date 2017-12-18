6ac1e08b5633559de0291526028c37ca0cf62fbe
Alexey Khodos <alexey.khodos@nexenta.com>
2017-12-18 12:41:28 -0800

    Newton/5x upgrade (#33)
    * added NS5 configurable target prefix
    
    * removed target-volume caching, ns5 isscsi portals and hostGroups support
    
    * nedge handle extend in create from snap
    
    * fixed KeyError if lun exists
    
    * improved debug logging

--- cinder/volume/drivers/nexenta/ns5/nfs.py
+++ cinder/volume/drivers/nexenta/ns5/nfs.py
@@ -519,7 +519,7 @@ class NexentaNfsDriver(nfs.NfsDriver):
         pool = self.share.split('/')[0]
         LOG.debug(
             'Creating ACL for filesystem %s on Nexenta Store', filesystem)
-        url = 'storage//filesystems/%s/acl' % (
+        url = 'storage/filesystems/%s/acl' % (
             '%2F'.join([pool, path.replace('/', '%2F'), filesystem]))
         data = {
             "type": "allow",
